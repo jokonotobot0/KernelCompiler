20a772fab553042767e1cfa82eef7449936dd96c
jokonotobot0 <166203936+jokonotobot0@users.noreply.github.com>
2024-04-29 07:30:04 +0700

    target.sh

--- target.sh
+++ target.sh
@@ -3,8 +3,8 @@
 # Copyright (C) 2021 a xyzprjkt property
 
 # Main
-KERNEL_ROOTDIR= # IMPORTANT ! Fill with your kernel source root directory.
-DEVICE_DEFCONFIG= # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_ROOTDIR= # IMPORTANT! Put your clang directory here.
-export KBUILD_BUILD_USER= # Change with your own name or else.
-export KBUILD_BUILD_HOST= # Change with your own hostname.
+KERNEL_ROOTDIR=$(pwd)RMX2189Q # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_DEFCONFIG=oppo6765_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
+CLANG_ROOTDIR=$(pwd)/proton # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER=jokonotoboto # Change with your own name or else.
+export KBUILD_BUILD_HOST=jokonotobot0 # Change with your own hostname.
